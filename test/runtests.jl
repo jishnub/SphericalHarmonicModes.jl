12d40e285b2e12f6934b1cebe58408b5738a1c6e
jishnub <jishnuonline@gmail.com>
2019-08-25 20:45:27 +0500

    fixed version number

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,19 +1,5 @@
 using Test,SphericalHarmonicModes
 
-# @testset "modes skipped" begin
-# 	for smax=0:10,smin=0:smax,tmin=-smax:smax,tmax=tmin:smax
-		
-# 		@test SphericalHarmonicModes.neg_skip(smin,smax,tmin,tmax) >= 0
-# 		@test SphericalHarmonicModes.pos_skip(smin,smax,tmin,tmax) >= 0
-
-# 		@test SphericalHarmonicModes.neg_skip(smin,smax,tmin,tmax) == 
-# 					count(t<tmin for s=smin:smax for t=-s:s)
-
-# 		@test SphericalHarmonicModes.pos_skip(smin,smax,tmin,tmax) == 
-# 					count(t>tmax for s=smin:smax for t=-s:s)
-# 	end
-# end
-
 @testset "constructors" begin
 
 	@testset "st" begin
